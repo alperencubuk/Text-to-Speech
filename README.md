39e2d01b6a606577d87cfbfe66a4f64cf26848bf
alperencubuk <alperencubuk@outlook.com>
2019-06-06 04:43:20 +0300

    Text to Speech

---
+++ README.md
@@ -0,0 +1,18 @@
+# Text to Speech
+
+### Summary:
+
+Convert "txt" to "mp3" with Google Translate text-to-speech API.
+
+### Requirements:
+
+- Python3 (3.6.8 Recommended)
+- gTTS (pip install gTTS)
+  
+### How to Run:
+
+- python text_to_speech.py
+
+---
+
+**Alperen Cubuk**
---
+++ text_to_speech.py
@@ -0,0 +1,14 @@
+﻿from gtts import gTTS
+
+def text_to_speech(input_name, output_name, language):
+    file = open(input_name, 'r')
+    content = file.read()
+    file.close()
+    sound = gTTS(text=content, lang=language)
+    sound.save(output_name + '.mp3')
+	#https://pypi.org/project/gTTS/
+	
+text_to_speech('input_en.txt', 'sound_en', 'en')
+#text_to_speech('input_tr.txt', 'sound_tr', 'tr')
+
+print('Done!')
